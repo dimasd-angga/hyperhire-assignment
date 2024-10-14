ce95e473e170efcc4fe53f6a12bb7a2192dc1dfe
dimskicoding <email.dimasdarfiangga@gmail.com>
2024-10-15 01:07:22 +0700

    fix env value

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,8 @@ import BannerFeaturedItem from '@/app/components/molecules/home-banner/BannerFea
 import BannerFeaturedItemCheck from '@/app/components/molecules/home-banner/BannerFeaturedItemCheck';
 
 async function fetchData(endpoint: string) {
-  const res = await fetch(`/api/banner/${endpoint}`, { cache: 'no-store' });
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+  const res = await fetch(`${apiUrl}/api/banner/${endpoint}`, { cache: 'no-store' });
 
   if (!res.ok) {
     throw new Error(`Failed to fetch ${endpoint} data`);
